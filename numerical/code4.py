724115b9521603ec459cd2c29324dd051b482d7e
Arafat <maneone984@gmail.com>
2025-08-16 21:06:14 +0600

    Create code4.py

---
+++ numerical/code4.py
@@ -0,0 +1,10 @@
+x = int(input("Enter number: "))
+if x > 1:
+    for i in range(2, x):
+        if x % i == 0:
+            print("Not Prime")
+            break
+    else:
+        print("Prime")
+else:
+    print("Not Prime")
